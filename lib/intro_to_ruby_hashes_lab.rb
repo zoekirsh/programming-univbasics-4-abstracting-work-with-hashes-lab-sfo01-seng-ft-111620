af9cb825bd3b28692749901d5323b0978c3f83fe
maxwellbenton <maxwellbenton@gmail.com>
2020-03-02 16:00:26 -0500

    move file to lib folder, remove part 1 methods

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,19 +1,3 @@
-def new_hash
-  # return an empty hash
-end
-
-def my_hash
-  # return a valid hash with any key/value pair of your choice
-end
-
-def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-end
-
-def id_generator
-  # return a hash with a key :id assigned to positive integer
-end
-
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
 end
