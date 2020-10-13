5869e07de1cc2e8fc53efc05cf35d6fb4fbe3b04
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-13 00:13:39 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,10 +9,6 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key] 
-    hash[key] += 1 
-  else 
-    hash[key] = 1 
-  end
-  hash 
-end
+  hash[key] ? hash[key] += 1 : hash[key] = 1 
+hash 
+end 
